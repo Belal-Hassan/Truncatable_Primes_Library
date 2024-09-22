ecd4a066d5034ad0f438ed37ae6819ac9346ef9d
Belal-Hassan <belal.34327@gmail.com>
2024-09-22 03:07:43 +0300

    Effeciency Update

--- TPs.cpp
+++ TPs.cpp
@@ -1,153 +1,164 @@
 #include "TPs.h"
-#include <iostream>
-#include <cmath>
-using namespace std;
 
-uint8 sizeofint(uint64 num)
+uint8 sizeofint(uint64 num) //output the number of digits of an integer.
 {
 	uint8 size = 0;
 	while (num > 0)
 	{
-		num /= 10;
-		size++;
+		num /= 10; 
+		size++; 
 	}
 	return size;
 }
-
-bool flip(uint64* num) //Flips a number (234 to 432) and returns true if it could be flipped
+bool flip(uint64* num) //flip the number via its pointer.
 {
-	uint64 size = 0, bu = *num;
-	while (bu > 0)
-	{
-		if (bu % 10 == 0)
-			return false;
-		bu /= 10;
-		size++;
-	}
-	for (int i = size - 1; i >= 0; i--)
-	{
-		bu += (*num % 10) * pow(10, i);
-		*num -= (*num % 10);
-		*num /= 10;
-	}
-	*num = bu;
+	if(*num % 10 == 0)
+		return false; //if the number ends in 0, it cannot be flipped.
+    uint64 flipped = 0;
+    while (*num > 0) {
+        int digit = *num % 10;  //get the last digit.
+        flipped = flipped * 10 + digit;  //build the flipped number.
+        *num /= 10;  //remove the last digit from the original number.
+    }
+    *num = flipped;  //store the flipped number back in the original variable via pointer.
 	return true;
 }
-
-bool isPrime(uint64 num) //Checks if a number is prime
+bool isPrime(uint64 num) //check if a number is prime.
 {
 	if (num == 1)
-		return false;
+		return false; 
 	if (num == 2)
 		return true;
-	for (uint64 i = 2; i <= ceil(sqrt(num)); i++)
-		if (num % i == 0)
+	for (uint64 i = 2; i <= ceil(sqrt(num)); i++) 
+		if (num % i == 0) //there exists at least a divisor less than the square root of the number if it's not prime.
 			return false;
 	return true;
 }
-
-bool isRTP(uint64 num) //Checks if a number is right-truncatable prime
+bool isRTP(uint64 num) //check if a number is a right-truncatable prime
 {
 	if (sizeofint(num) > 8)
-		return false;
-	while (num > 0)
+		return false; //return false because there are no right-truncatable primes with more than 8 digits.
+	while (num > 0) //check if the number is prime after removing each rightmost digit.
 	{
 		if (!isPrime(num))
 			return false;
 		else
 		{
-			num -= (num % 10);
-			num /= 10;
+			num -= (num % 10); 
+			num /= 10; 
 		}
 	}
 	return true;
 }
-
-bool isLTP(uint64 num) //Checks if the a is a left-truncatable prime
+bool isLTP(uint64 num) //Check if the a is a left-truncatable prime.
 {
-	if (sizeofint(num) > 24)
-		return false;
-	while (num > 0)
+	if (sizeofint(num) > 19)
+		return false; //The limit of the unsigned long long is 19 digits.
+	while (num > 0) //check if the number is prime after removing each leftmost digit.
 	{
 		if (!isPrime(num))
 			return false;
-		if (!flip(&num))
+		if (!flip(&num)) 
 			return false;
-		num -= (num % 10);
-		num /= 10;
-		flip(&num);
+		num -= (num % 10); 
+		num /= 10; 
+		flip(&num); 
 	}
 	return true;
 }
-
-bool isTP(uint64 num) //Checks if a number is a right-truncatable prime & a left-truncatable prime at the same time
+bool isTSP(uint64 num) //check if a number is a right-truncatable prime & a left-truncatable prime at the same time (two-sided prime).
 {
 	if (sizeofint(num) > 6)
-		return false;
-	if (isRTP(num) && isLTP(num))
+		return false; //return false because there are no two-sided primes with more than 6 digits.
+	if (isRTP(num) && isLTP(num)) 
 		return true;
 	return false;
 }
-
-bool isLRTP(uint64 num)
+uint64 rightNumberCombined(uint64 mainNumber, uint8 additionalNumber) //adds the additionalNumber to the right of the mainNumber.
 {
-	if (sizeofint(num) > 97)
-		return false;
-	uint64 ph = num;
-	while (num > 0)
-	{
-		if (!isPrime(num))
-			return false;
-		else
-		{
-			num -= (num % 10);
-			num /= 10;
-		}
-		if (!flip(&num))
-			return false;
-		num -= (num % 10);
-		num /= 10;
-		flip(&num);
-	}
-	return true;
+  	return (mainNumber * 10) + additionalNumber; 
 }
-
-void RTPs(uint8 n) //Outputs all n-digit right-truncatable primes
+uint64 leftNumberCombined(uint64 mainNumber, uint8 additionalNumber) //adds the additionalNumber to the left of the mainNumber.
+{
+	uint64 cast = mainNumber; //cast in order not to manipulate the mainNumber.
+	flip(&cast); 
+	uint64 output = (cast * 10) + additionalNumber; 
+	flip(&output); 
+  	return output;
+}
+vect64 RTPs(uint8 n) //output all n-digit right-truncatable primes.
 {
 	if (n > 8)
-		return;
-	uint64 begin = pow(10, n - 1) + 1, end = pow(10, n) - 3;
-	for (int num = begin; num <= end; num += 2)
-		if (isRTP(num))
-			cout << num << endl;
+    	return vect64(); //return an empty vector because there are no right-truncatable primes with more than 8 digits.
+	vect64 RTPvector;
+  	uint64 tempNumber;
+  	uint16 vectSize = 0;
+  	copy(&primes[0], &primes[4], back_inserter(RTPvector)); //start with a vector of primes to build up upon it.
+  	for(uint8 counter = 1; counter < n; counter++)
+  	{
+    	vectSize = RTPvector.size();
+    	for(uint16 vectCounter = 0; vectCounter < vectSize; vectCounter++)
+    	{
+      		for (uint8 oddCounter = 0; oddCounter < 4; oddCounter++)
+      		{
+        		tempNumber = rightNumberCombined(RTPvector[vectCounter], odds[oddCounter]); 
+        		if(isPrime(tempNumber))
+          			RTPvector.push_back(tempNumber);
+      		}
+    	}
+    	RTPvector.erase(RTPvector.begin(), RTPvector.begin() + vectSize);
+  	}
+	RTPvector.erase(remove_if(RTPvector.begin(), RTPvector.end(), [](uint64 num) {return !isRTP(num);}), RTPvector.end());
+	return RTPvector;
 }
-
-void LTPs(uint8 n) //Outputs all n-digit left-truncatable primes
+vect64 LTPs(uint8 n) //outputs all n-digit left-truncatable primes.
 {
-	if (n > 24)
-		return;
-	uint64 begin = pow(10, n - 1) + 1, end = pow(10, n) - 1;
-	for (int num = begin; num <= end; num++)
-		if (isLTP(num))
-			cout << num << endl;
+	if (n > 19)
+		return vect64();
+    vect64 LTPvector;
+  	uint64 tempNumber;
+  	uint16 vectSize = 0;
+  	copy(&LTprimes[0], &LTprimes[2], back_inserter(LTPvector));
+  	for(uint8 counter = 1; counter < n; counter++)
+  	{
+    	vectSize = LTPvector.size();
+    	for(uint32 vectCounter = 0; vectCounter < vectSize; vectCounter++)
+    	{
+      		for (uint8 number = 1; number < 10; number++)
+      		{
+        		tempNumber = leftNumberCombined(LTPvector[vectCounter], number);
+        		if(isPrime(tempNumber))
+          			LTPvector.push_back(tempNumber);
+      		}
+    	}
+    	LTPvector.erase(LTPvector.begin(), LTPvector.begin() + vectSize);
+  	}
+	LTPvector.erase(remove_if(LTPvector.begin(), LTPvector.end(), [](uint64 num) {return !isLTP(num);}), LTPvector.end());
+	return LTPvector;
 }
-
-void TPs(uint8 n) //Outputs all n-digit numbers that are right truncatable primes and left truncatable primes at the same time.
+vect64 TSPs(uint8 n) //Outputs all n-digit numbers that are right truncatable primes and left truncatable primes at the same time (Two-sided Primes).
 {
 	if (n > 6)
-		return;
-	uint64 begin = pow(10, n - 1) + 1, end = pow(10, n) - 3;
-	for (int num = begin; num <= end; num += 2)
-		if (isTP(num))
-			cout << num << endl;
-}
-
-void LRTPs(uint8 n) //Outputs all n-digit right-and-left truncatable primes.
-{
-	if (n > 97)
-		return;
-	uint64 begin = pow(10, n - 1) + 1, end = pow(10, n) - 3;
-	for (int num = begin; num <= end; num += 2)
-		if (isLRTP(num))
-			cout << num << endl;
+		return vect64();
+    vect64 TSPvector;
+  	uint64 tempNumber;
+  	uint16 vectSize = 0;
+  	copy(&primes[0], &primes[4], back_inserter(TSPvector));
+  	for(uint8 counter = 1; counter < n; counter++)
+  	{
+    	vectSize = TSPvector.size();
+    	for(uint16 vectCounter = 0; vectCounter < vectSize; vectCounter++)
+    	{
+      		for (uint8 oddCounter = 0; oddCounter < 4; oddCounter++)
+      		{
+        		tempNumber = rightNumberCombined(TSPvector[vectCounter], odds[oddCounter]);
+        		if(isPrime(tempNumber))
+          			TSPvector.push_back(tempNumber);
+      		}
+    	}
+    	TSPvector.erase(TSPvector.begin(), TSPvector.begin() + vectSize);
+  	}
+	TSPvector.erase(remove_if(TSPvector.begin(), TSPvector.end(), [](uint64 num) {return !isRTP(num);}), TSPvector.end());
+  	TSPvector.erase(remove_if(TSPvector.begin(), TSPvector.end(), [](uint64 num) {return !isLTP(num);}), TSPvector.end());
+	return TSPvector;
 }
--- TPs.h
+++ TPs.h
@@ -1,15 +1,33 @@
 #pragma once
 
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <bits/stdc++.h>
+#define fpow(x, y) round(pow(x, y))
+using namespace std;
+
+/*_______________data types definition_______________*/
 typedef unsigned char uint8;
+typedef unsigned short uint16;
 typedef unsigned int uint32;
 typedef unsigned long long int uint64;
+typedef vector<uint64> vect64;
 
-bool isPrime(uint64 num);
-bool isRTP(uint64 num);
-bool isLTP(uint64 num);
-bool isTP(uint64 num);
-bool isLRTP(uint64 num);
-void RTPs(uint8 size);
-void LTPs(uint8 size);
-void TPs(uint8 size);
-void LRTPs(uint8 size);
+/*_______________functions' decleration_______________*/
+uint8 sizeofint(uint64 num); //output the number of digits of an integer.
+bool flip(uint64* num); //flip the number via its pointer.
+bool isPrime(uint64 num); //check if a number is prime.
+bool isRTP(uint64 num); //check if a number is a right-truncatable prime.
+bool isLTP(uint64 num); //Check if the a is a left-truncatable prime.
+bool isTSP(uint64 num); //check if a number is a right-truncatable prime & a left-truncatable prime at the same time (two-sided prime).
+uint64 rightNumberCombined(uint64 mainNumber, uint8 additionalNumber); //adds the additionalNumber to the right of the mainNumber.
+uint64 leftNumberCombined(uint64 mainNumber, uint8 additionalNumber); //adds the additionalNumber to the left of the mainNumber.
+vect64 RTPs(uint8 size); //output all n-digit right-truncatable primes.
+vect64 LTPs(uint8 size); //outputs all n-digit left-truncatable primes.
+vect64 TSPs(uint8 size); //Outputs all n-digit numbers that are right truncatable primes and left truncatable primes at the same time (Two-sided Primes).
+
+/*_______________some useful arrays_______________*/
+const char odds[4] = {1, 3, 7, 9}; //the odd numbers that could be added to the right of primes.
+const char LTprimes[2] = {3, 7};
+const char primes[4] = {2, 3, 5, 7};
